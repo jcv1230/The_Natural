65a2574c273fe3877bbe2e606feddab0958e0836
Juan Camilo Valencia <camilo.valencia@utp.edu.co>
2023-12-15 19:08:59 -0500

    PROYECTO FINAL

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 from jwt_manager import create_token, validate_token
 from fastapi.security import HTTPBearer
 from config.database import Session, engine, Base
-from models.product import Product as ProductModel, Category as CategoryModel
+from models.product import Product as ProductModel, Category as CategoryModel, Supplier as SupplierModel
 from fastapi.encoders import jsonable_encoder
 from typing import Optional, List
 
@@ -22,27 +22,38 @@ class JWTBearer(HTTPBearer):
         if data['email'] != "admin@gmail.com":
             raise HTTPException(status_code=401, detail="Invalid user")
         
+# MODELOS DE DATOS
+
 class User(BaseModel):
     email:str
     password:str 
     
 class Product(BaseModel):
     id: int = Field(default=0, title = "ID del Producto")
+    supplier_id: int = Field(default=0, title = "ID del Proveedor")
     name: str = Field(default="----", min_length=2, max_length=50, title = "Nombre del Producto")
     description: str = Field(default="-----", min_length=10, max_length=300, title = "Descripcion del Producto")
     price: float = Field(default=0, ge=0, title = "Precio del Producto" )
-    category: str = Field(default="----", min_length=4, max_length=15, title = "Categoria del Producto")
+    sub_category: str = Field(default="----", min_length=4, max_length=15, title = "Categoria del Producto")
 
 class Category(BaseModel):
-    category: str = Field(default="----", min_length=4, max_length=15, title = "Categoria del Producto")
+    categories: str = Field(default="----", min_length=4, max_length=15, title = "Categoria del Producto")
     sub_category: str = Field(default="----", min_length=4, max_length=15, title = "Sub Categoria del Producto")
-
-# Ruta para el Home
+    
+class Supplier(BaseModel):
+    supplier_id: int = Field(default=0, title = "ID del Proveedor")
+    name: str = Field(default="----", min_length=2, max_length=50, title = "Nombre del Proveedor")
+    address: str = Field(default="-----", min_length=10, max_length=300, title = "Direccion del Proveedor")
+    phone: int = Field(default=0, ge=0, title = "Telefono del Proveedor" )
+    email: str = Field(default="----", min_length=4, max_length=15, title = "Email del Proveedor")
+    city: str = Field(default="----", min_length=4, max_length=15, title = "Ciudad del Proveedor")
+    
+# Ruta para el HOME
 @API.get("/", tags=['Home'])
 def message():
     return HTMLResponse(content="<h1> Market Place THE NATURAL</h1>")
 
-# Ruta para el Login
+# Ruta para el LOGIN
 @API.post("/login", tags=['auth'], response_model=dict, status_code=200)
 def login(user: User):
     if user.email == "admin@gmail.com" and user.password == "admin":
@@ -50,13 +61,31 @@ def login(user: User):
         return JSONResponse(content={"token": token}, status_code=200)
     else:
         return JSONResponse(content={"message": "Invalid crentials"}, status_code=401)
-    
+
+# OBTENER TODAS LAS TABLAS RELACIONADAS
+
 # Get para obtener todos los productos
 @API.get("/Product", tags=['Products'], response_model=List[Product], status_code=200) 
 def get_product() -> List[Product]:
     db = Session()
-    result = db.query(ProductModel).all() # SELECT * FROM movies
+    result = db.query(ProductModel).all() 
     return JSONResponse(status_code=200, content=jsonable_encoder(result))
+
+# Get para obtener todos las categorias
+@API.get("/Category", tags=['Category'], response_model=List[Category], status_code=200) 
+def get_category() -> List[Category]:
+    db = Session()
+    result = db.query(CategoryModel).all() 
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+
+#Get para obtener todos los proveedores
+@API.get("/Supplier", tags=['Supplier'], response_model=List[Supplier], status_code=200)
+def get_supplier() -> List[Supplier]:
+    db = Session()
+    result = db.query(SupplierModel).all() 
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+
+# FILTRAR LAS TABLAS RELACIONADAS
 
 # Get filtrando por categoria los productos
 @API.get("/Product/{sub_category}", tags=['Products'], status_code=200, response_model=Product)
@@ -67,24 +96,29 @@ def get_product_by_sub_category(sub_category: str) -> Product:
         return JSONResponse(content=jsonable_encoder(result), status_code=200)
     else:
         return JSONResponse(content={"message": "Movie not found"}, status_code=404)
-    
-# Get para obtener todos las categorias
-@API.get("/Category", tags=['Category'], response_model=List[Category], status_code=200) 
-def get_category() -> List[Category]:
-    db = Session()
-    result = db.query(CategoryModel).all() 
-    return JSONResponse(status_code=200, content=jsonable_encoder(result))
 
-# Get filtrando subcategorias por categoria
-@API.get("/Category/{sub_category}", tags=['Category'], status_code=200, response_model=Category)
-def get_category_by_sub_category(sub_category: str) -> Category:
+# Get filtrando por categoria principal las categorias
+@API.get("/Category/{categories}", tags=['Category'], status_code=200, response_model=Category)
+def get_category_by_categories(categories: str) -> Category:
     db = Session()
-    result = db.query(CategoryModel).filter(ProductModel.sub_category == sub_category ).first()
+    result = db.query(CategoryModel).filter(CategoryModel.categories == categories ).first()
     if result:
         return JSONResponse(content=jsonable_encoder(result), status_code=200)
     else:
         return JSONResponse(content={"message": "Movie not found"}, status_code=404)
     
+# Get filtrando por ciudad los proveedores
+@API.get("/Supplier/{city}", tags=['Supplier'], status_code=200, response_model=Supplier)
+def get_supplier_by_city(city: str) -> Supplier:
+    db = Session()
+    result = db.query(SupplierModel).filter(SupplierModel.city == city ).first()
+    if result:
+        return JSONResponse(content=jsonable_encoder(result), status_code=200)
+    else:
+        return JSONResponse(content={"message": "Movie not found"}, status_code=404)
+    
+# CREAR PRODUCTOS, CATEGORIAS Y PROVEEDORES
+
 # Post para crear un producto
 @API.post("/Product", tags=['Products'], response_model=dict, status_code=201)
 def create_product(product: Product) -> dict:
@@ -105,6 +139,18 @@ def create_category(category: Category) -> dict:
     return JSONResponse(content={"message": "Category created successfully"},
                         status_code=201)
     
+# Post para crear un proveedor
+@API.post("/Supplier", tags=['Supplier'], response_model=dict, status_code=201)
+def create_supplier(supplier: Supplier) -> dict:
+    db = Session()
+    new_supplier = SupplierModel(**supplier.model_dump())
+    db.add(new_supplier)
+    db.commit()
+    return JSONResponse(content={"message": "Supplier created successfully"},
+                        status_code=201)
+    
+# ACTUALIZAR PRODUCTOS, CATEGORIAS Y PROVEEDORES
+    
 # Put para actualizar un producto
 @API.put("/Product/{id}", tags=['Products'], response_model=dict, status_code=200)
 def update_product(id: int, product: Product) -> dict:
@@ -112,10 +158,11 @@ def update_product(id: int, product: Product) -> dict:
     result = db.query(ProductModel).filter(ProductModel.id == id).first()
     if not result:
         return JSONResponse(content={"message": "Product not found"}, status_code=404)
+    result.supplier_id = product.supplier_id
     result.name = product.name
     result.description = product.description
     result.price = product.price
-    result.category = product.category
+    result.sub_category = product.sub_category
     db.commit()
     return JSONResponse(content={"message": "Product updated successfully"}, status_code=200)
 
@@ -130,6 +177,23 @@ def update_category(sub_category: str, category: Category) -> dict:
     result.sub_category = category.sub_category
     db.commit()
     return JSONResponse(content={"message": "Category updated successfully"}, status_code=200)
+
+# Put para actualizar un proveedor
+@API.put("/Supplier/{supplier_id}", tags=['Supplier'], response_model=dict, status_code=200)
+def update_supplier(supplier_id: int, supplier: Supplier) -> dict:
+    db = Session()
+    result = db.query(SupplierModel).filter(SupplierModel.supplier_id == supplier_id).first()
+    if not result:
+        return JSONResponse(content={"message": "Supplier not found"}, status_code=404)
+    result.name = supplier.name
+    result.address = supplier.address
+    result.phone = supplier.phone
+    result.email = supplier.email
+    result.city = supplier.city
+    db.commit()
+    return JSONResponse(content={"message": "Supplier updated successfully"}, status_code=200)
+
+# ELIMINAR PRODUCTOS, CATEGORIAS Y PROVEEDORES
 
 # Delete para eliminar un producto
 @API.delete("/Product/{id}", tags=['Products'], response_model=dict)
@@ -153,6 +217,16 @@ def delete_category(sub_category: str) -> dict:
     db.commit()
     return JSONResponse(content={"message": "Category deleted successfully"})
 
+# Delete para eliminar un proveedor
+@API.delete("/Supplier/{supplier_id}", tags=['Supplier'], response_model=dict)
+def delete_supplier(supplier_id: int) -> dict:
+    db = Session()
+    result = db.query(SupplierModel).filter(SupplierModel.supplier_id == supplier_id).first()
+    if not result:
+        return JSONResponse(content={"message": "Supplier not found"}, status_code=404)
+    db.delete(result)
+    db.commit()
+    return JSONResponse(content={"message": "Supplier deleted successfully"})
 
 
     
--- models/product.py
+++ models/product.py
@@ -1,19 +1,34 @@
 from config.database import Base
 from sqlalchemy import Column, Integer, Float, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 class Product(Base):
     __tablename__ = "Products"
     id = Column(Integer, primary_key=True, index=True)
+    supplier_id = Column(Integer, ForeignKey('Suppliers.supplier_id'))  # Esto es una clave foránea
     name = Column(String(50), nullable=False)
     description = Column(String(300), nullable=False)
     price = Column(Float, nullable=False)
-    sub_category = Column(String, primary_key=True, nullable=False)
+    sub_category = Column(String, ForeignKey('Categories.sub_category'))  # Esto es una clave foránea
+    #category = relationship("Category", back_populates="products")  # Esto es una relación muchos a uno
+    #supplier = relationship("Supplier", back_populates="products")  # Esto es una relación muchos a uno
     
 class Category(Base):
     __tablename__ = "Categories"
-    categories = Column(String(50), nullable=False)
+    categories = Column(String, nullable=False)
     sub_category = Column(String, nullable=False, primary_key=True)
+    #products = relationship("Product", back_populates="sub_category")  # Esto es una relación uno a muchos
     
+class Supplier(Base):
+    __tablename__ = "Suppliers"
+    supplier_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), nullable=False)
+    address = Column(String(300), nullable=False)
+    phone = Column(Integer, nullable=False)
+    email = Column(String(50), nullable=False)
+    city = Column(String(50), nullable=False)
+    #products = relationship("Product", back_populates="Supplier_id")  # Esto es una relación uno a muchos
 
 
    
